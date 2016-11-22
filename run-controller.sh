34dd721aacc6351c71de8b4a5b990c39c2d5abf5
Paul Roche <paul@dreamisle.ca>
2016-11-21 22:45:57 -0800

    expose 6789 as used for speed test

--- run-controller.sh
+++ run-controller.sh
@@ -4,7 +4,7 @@ set +x
 
 
 CONTAINER_NAME="unifi-controller"
-IMAGE_NAME="dreamisle/unifi-controller"
+IMAGE_NAME="dreamisle.ca/unifi-controller"
 
 
 docker stop $CONTAINER_NAME || echo "cannot stop a non running $CONTAINER_NAME"
@@ -13,6 +13,7 @@ docker rm -f $CONTAINER_NAME || echo "not a container yet: '$CONTAINER_NAME'"
 # 8880 8080 8443 37117 
 
 docker run -d \
+        -p 6789:6789 \
 	-p 8080:8080 \
         -p 8880:8880 \
 	-p 8443:8443 \
